7f199a331aa9b9811495519c2e1c8d592f5a2e0b
Nana Lau <nanalau315@hotmail.com>
2020-11-09 03:13:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,16 +6,21 @@ jap_emote = YAML.load_file('./lib/emoticons.yml')
 # jap_emote = ./lib/emoticons.yml
 
 def load_library(file_path)
-  # code goes here
+  
   
   
   
 end
+
+
 
 def get_japanese_emoticon
-  # code goes here
+
 end
 
+
+
+
 def get_english_meaning
-  # code goes here
+
 end
