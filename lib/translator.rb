dc4451520210f7084c49bb415a6aa809444f8041
Nana Lau <nanalau315@hotmail.com>
2020-11-09 04:30:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,7 @@ def get_english_meaning(path, emote)
   emote_hash = load_library(path)
   emote_hash.each do |emote_name, e|
     if e[:japanese] == emote
-    return e
+    return emote_name
   end
   end
   
