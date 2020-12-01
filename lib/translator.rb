e787bf9881d3f75a92b513063f4ab11817ede30d
nanalau315 <71403709+nanalau315@users.noreply.github.com>
2020-11-30 21:21:19 -0500

    Update translator.rb

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,25 +10,21 @@ def load_library(path)
 end
 
 def get_english_meaning(path, emote)
-  sorry_message = "Sorry, that emoticon was not found"
-  
   emote_hash = load_library(path)
   emote_hash.each do |emote_name, e|
     if e[:japanese] == emote
     return emote_name
     end
   end
-  sorry_message
+  "Sorry, that emoticon was not found"
 end
 
 def get_japanese_emoticon(path, emote)
-  sorry_message = "Sorry, that emoticon was not found"
-  
   emote_hash = load_library(path)
   emote_hash.each do |emote_name, e|
     if e[:english] == emote
       return e[:japanese]
     end
   end
-  sorry_message
+  "Sorry, that emoticon was not found"
 end
